44b32d0fc6b37a9f628f0390a77ecd1110c5b616
Sathwik <96546441+sathwikkannam@users.noreply.github.com>
2022-03-04 21:31:27 +0100

    refreshed and updated task 2 to 5

--- Programming for Embedded Systems/Labs/Lab 2/Task 3/Task 3/main.asm
+++ Programming for Embedded Systems/Labs/Lab 2/Task 3/Task 3/main.asm
@@ -3,32 +3,34 @@
 ;
 ; Created: 2022-02-23 14:11:05
 ; Author : SAKA0191
-;
-
-
 ;Write a short program that adds two integer values, where the sum is less 
 ;than or equal to 9, and display the result on a 7-segment display
+
 .include "m328pdef.inc"
 
+.equ zero          =               0
+.equ one           =               1
+.equ two           =               2
+.equ three         =               3
+.equ four          =               4
+.equ five          =               5
+.equ six           =               6
+.equ seven         =               7
+.equ eight         =               8
+.equ nine          =               9
+.equ DDRB_PINS     =               0b00001111
+.equ DDRD_PINS     =               0b11110000
+.equ PB            =               0x25
+.equ PD            =               0x2B
+
 start:
-	LDI R17, 0b00001111 
+	LDI R17, DDRB_PINS 
 	STS 0x24, R17 ;; DDRB OUTPUT
 
-	LDI R18, 0b11110000 
+	LDI R18, DDRD_PINS 
 	STS 0x2A, R18 ;; DDRD OUTPUT
 
-	LDI R20, 1
-	LDI R21, 2
-	LDI R22, 3
-	LDI R23, 4
-	LDI R24, 5
-	LDI R25, 6
-	LDI R26, 7
-	LDI R27, 8
-	LDI R28, 9
-	LDI R29, 0
-
-	LDI R19, 0
+	LDI R19, 0 ;; R19 and R16 as inputs
 	LDI R16, 0
 	ADD R19, R16  ;; MUL R19, R16 or SUB R19, R16
 	CP R19, R28
@@ -37,104 +39,103 @@ start:
 	BRGE not_equal_or_higher
 
 equal_or_lower:
-	CP R19, R29
+	CPI R19, R29
 	BREQ not_equal_or_higher
-	CP R19, R20
+	CPI R19, one
 	BREQ turn_on_one
-	CP R19, R21
+	CPI R19, two
 	BREQ turn_on_two
-	CP R19, R22
+	CPI R19, three
 	BREQ turn_on_three
-	CP R19, R23
+	CPI R19, four
 	BREQ turn_on_four
-	CP R19, R24
+	CPI R19, five
 	BREQ turn_on_five
-	CP R19, R25
+	CPI R19, six
 	BREQ turn_on_six
-	CP R19, R26
+	CPI R19, seven
 	BREQ turn_on_seven
-	CP R19, R27
+	CPI R19, eight
 	BREQ turn_on_eight
-	CP R19, R28
+	CPI R19, nine
 	BREQ turn_on_nine
 
 not_equal_or_higher: ;show 0 if higher of greater 
 	LDI R17, 0b00001101
-	STS 0x25, R17
+	STS PB, R17
 
 	LDI R18, 0b11110000
-	STS 0x2B, R18
-	
+	STS PD, R18
 	
 turn_on_one:
 	LDI R17, 0b00001001
-	STS 0x25, R17
+	STS PB, R17
 	rjmp loop
 
 turn_on_two:
 	LDI R17, 0b0001110
-	STS 0x25, R17
+	STS PB, R17
 
 	LDI R18, 0b11000000
-	STS 0x2B, R18
+	STS PD, R18
 	rjmp loop
 
 turn_on_three:
 
 	LDI R17, 0b0000110
-	STS 0x25, R17
+	STS PB, R17
 
 	LDI R18, 0b11110000
-	STS 0x2B, R18
+	STS PD, R18
 	rjmp loop
 
 turn_on_four:
 	LDI R17, 0b00000011
-	STS 0x25, R17
+	STS PB, R17
 
 	LDI R18, 0b01100000
-	STS 0x2B, R18
+	STS PD, R18
 	rjmp loop
 
 turn_on_five:
 	LDI R17, 0b00000111
-	STS 0x25, R17
+	STS PB, R17
 
 	LDI R18, 0b10110000
-	STS 0x2B, R18
+	STS PD, R18
 	rjmp loop
 
 turn_on_six:
 	LDI R17, 0b00001111
-	STS 0x25, R17
+	STS PB, R17
 
 	LDI R18, 0b10110000
-	STS 0x2B, R18
+	STS PD, R18
 	rjmp loop
 
 turn_on_seven:
 	LDI R17, 0b00000000
-	STS 0x25, R17
+	STS PB, R17
 
 	LDI R18, 0b11110000
-	STS 0x2B, R18
+	STS PD, R18
 	rjmp loop
 
 turn_on_eight:
 
 	LDI R17, 0b00001111
-	STS 0x25, R17
+	STS PB, R17
 
 	LDI R18, 0b11110000
-	STS 0x2B, R18
+	STS PD, R18
 	rjmp loop
 
 turn_on_nine:
 	LDI R17, 0b00000111
-	STS 0x25, R17
+	STS PB, R17
 
 	LDI R18, 0b11110000
-	STS 0x2B, R18
+	STS PD, R18
 	rjmp loop
 
 loop: ;; loop to prevent jumping to random registers.
---
+++ Programming for Embedded Systems/Labs/Lab 2/Task 4/Task 4/main.asm
@@ -0,0 +1,196 @@
+
+.include "m328pdef.inc"
+
+.equ zero          =               0
+.equ one           =               1
+.equ two           =               2
+.equ three         =               3
+.equ four          =               4
+.equ five          =               5
+.equ six           =               6
+.equ seven         =               7
+.equ eight         =               8
+.equ nine          =               9
+.equ DDRB_PINS     =               0b00001111
+.equ DDRD_PINS     =               0b11110000
+.equ PB            =               0x25
+.equ PD            =               0x2B
+
+LDI R20, zero
+LDI R21, one
+LDI R22, two
+LDI R23, three
+LDI R24, four
+LDI R25, five
+LDI R26, six
+LDI R27, seven
+LDI R28, eight
+LDI R29, nine
+
+start:
+	CP R21, R20
+	BRGE a
+	CP R22, R20
+	BRGE b
+	CP R23, R20
+	BRGE c
+	CP R24, R20
+	BRGE d
+	CP R25, R20
+	BRGE e
+	CP R26, R20
+	BRGE f
+	CP R27, R20
+	BRGE g
+	CP R28, R20
+	BRGE h
+	CP R29, R20
+	BRGE i
+	rjmp display
+a:
+	MOV R20, R21
+	LDI R21, 0
+	rjmp start
+
+b:
+	MOV R20, R22
+	LDI R22, 0
+	rjmp start
+c:
+	MOV R20, R23
+	LDI R23, 0
+	rjmp start
+d:
+	MOV R20, R24
+	LDI R24, 0
+	rjmp start
+e:
+	MOV R20, R25
+	LDI R25, 0
+	rjmp start
+f:
+	MOV R20, R26
+	LDI R26, 0
+	rjmp start
+g:
+	MOV R20, R27
+	LDI R27, 0
+	rjmp start
+
+h:
+	MOV R20, R28
+	LDI R28, 0
+	rjmp start
+
+i:
+	MOV R20, R29
+	LDI R29, 0
+	rjmp start
+
+display:
+	LDI R17, DDRB_PINS
+	STS 0x24, R17
+
+	LDI R18, DDRD_PINS 
+	STS 0x2A, R18 
+
+	CPI R20, zero
+	BREQ turn_on_zero
+	CPI R20, one
+	BREQ turn_on_one
+	CPI R20, two
+	BREQ turn_on_two
+	CPI R20, three
+	BREQ turn_on_three
+	CPI R20, four
+	BREQ turn_on_four
+	CPI R20, five
+	BREQ turn_on_five
+	CPI R20, six
+	BREQ turn_on_six
+	CPI R20, seven
+	BREQ turn_on_seven
+	CPI R20, eight
+	BREQ turn_on_eight
+	CPI R20, nine
+	BREQ turn_on_nine
+
+turn_on_zero:
+	LDI R17, 0b00001101
+	STS PB, R17
+
+	LDI R18, 0b11110000
+	STS PD, R18
+	rjmp loop
+	
+turn_on_one:
+	LDI R17, 0b00001001
+	STS PB, R17
+	rjmp loop
+
+turn_on_two:
+	LDI R17, 0b0001110
+	STS PB, R17
+
+	LDI R18, 0b11000000
+	STS PD, R18
+	rjmp loop
+
+turn_on_three:
+	LDI R17, 0b0000110
+	STS PB, R17
+
+	LDI R18, 0b11110000
+	STS PD, R18
+	rjmp loop
+
+turn_on_four:
+	LDI R17, 0b00000011
+	STS PB, R17
+
+	LDI R18, 0b01100000
+	STS PD, R18
+	rjmp loop
+
+turn_on_five:
+	LDI R17, 0b00000111
+	STS PB, R17
+
+	LDI R18, 0b10110000
+	STS PD, R18
+	rjmp loop
+
+turn_on_six:
+	LDI R17, 0b00001111
+	STS PB, R17
+
+	LDI R18, 0b10110000
+	STS PD, R18
+	rjmp loop
+
+turn_on_seven:
+	LDI R17, 0b00000000
+	STS PD, R17
+
+	LDI R18, 0b11110000
+	STS PD, R18
+	rjmp loop
+
+turn_on_eight:
+	LDI R17, 0b00001111
+	STS PB, R17
+
+	LDI R18, 0b11110000
+	STS PD, R18
+	rjmp loop
+
+turn_on_nine:
+	LDI R17, 0b00000111
+	STS PB, R17
+
+	LDI R18, 0b11110000
+	STS PD, R18
+	rjmp loop
+
+loop: 
+	rjmp loop
