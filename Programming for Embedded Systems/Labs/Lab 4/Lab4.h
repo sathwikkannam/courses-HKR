b29d79231df25d8e796f4bff657beea88e372149
Sathwik <96546441+sathwikkannam@users.noreply.github.com>
2022-03-21 17:39:07 +0100

    Finished doPracticse()

--- Programming for Embedded Systems/Labs/Lab 4/Lab4.h
+++ Programming for Embedded Systems/Labs/Lab 4/Lab4.h
@@ -5,6 +5,14 @@
 #ifndef LAB_4_LAB4_H
 #define LAB_4_LAB4_H
 
+#define numberOfQuestions 10
+#define numberofTestQuestions 15
 
+int getInput();
+void doTest();
+long int getRandomNum();
+void doPractice(bool isSub, bool isAdd);
+int getRandomSignValue();
+void generateNumbers(bool isSub, bool isBoth, int numbers[3]);
 
 #endif //LAB_4_LAB4_H
--- Programming for Embedded Systems/Labs/Lab 4/main.c
+++ Programming for Embedded Systems/Labs/Lab 4/main.c
@@ -1,95 +1,118 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include "Lab4.h"
 
-
-#define numberOfQuestions 10
-#define numberofTestQuestions 15
-
-int getInput();
-void doTest();
-long int getRandomNum();
-void addition();
-void subtraction();
-int getRandomSign();
-void both();
 
 int main() {
-    int choice = getInput();
     int practiceChoice;
+    int choice = getInput();
 
     if(choice == 1){
-        scanf("1. Additions\n2. Subtractions\n3. Additions and Subtraction\nEnter your choice:%d", &practiceChoice);
+        printf("\n1. Additions\n2. Subtractions\n3. Additions and Subtraction\nEnter your choice:");
+        scanf("%d", &practiceChoice);
 
         if(practiceChoice == 1){
-            addition();
+            doPractice(false, true);
         }else if(practiceChoice == 2){
-            subtraction();
+            doPractice(true, false);
         }else if(practiceChoice == 3){
-            both();
+            doPractice(true, true);
         }
-
+        main();
     }else if(choice == 2){
         doTest();
+        main();
     }
     return 0;
 }
 
 
 int getInput(){
-    int name;
     int choice;
 
-    scanf("Enter name: %s\n", &name);
-    printf("Welcome, %s\n!", name);
+    printf("1. Practices\n2. Test\n3. Exit\nEnter your choice:");
+    scanf("%d", &choice);
 
-    scanf("1. Practices\n2. Test\n3. Exit\nEnter your choice:%d", &choice);
-    printf("\n");
+
     return choice;
 }
 
 
-void doTest(){
-
-
-}
-
-long int getRandomNum(){
-    return rand() %60;
-}
-
-int getRandomSign(){
-    return rand() %1;
-}
-
-
-
-void addition(){
-    int correctAnswer = 0;
+void doPractice(bool isSub, bool isAdd){
+    int totalCorrectAnswers = 0;
     int answer;
     int sum;
     int firstNum;
     int secondNum;
+    int signValue;
+    char sign;
+    int numbers[4];
 
-    while(correctAnswer != numberOfQuestions){
-        firstNum = getRandomNum();
-        secondNum = getRandomNum();
-        sum = firstNum + secondNum;
-        printf("\n\t%d. \t%d + %d = \n", correctAnswer+1, firstNum, secondNum);
-        scanf("Enter your answer: %d", &answer);
+    while(totalCorrectAnswers != numberOfQuestions){
+        generateNumbers(isSub, isAdd, numbers);
+        firstNum = numbers[0];
+        secondNum = numbers[1];
+        signValue = numbers[2];
+        sum = numbers[3];
+        sign = (signValue == 1)? '+' : '-';
 
-        if(answer == sum){
-            correctAnswer++;
-        }else{
-            scanf("No. Please try again. Enter ");
+        printf("\n\t%d.\t%d %c %d = \n", totalCorrectAnswers+1, firstNum, sign, secondNum);
+
+        while(answer != sum){
+            printf("Enter your answer: ");
+            scanf("%d", &answer);
+            printf("\n");
+
+            if(answer == sum){
+                totalCorrectAnswers++;
+            }else{
+                printf("No. Please try again.\n");
+            }
         }
+
     }
-}
-
-void subtraction(){
-    unsigned int correctAnswer;
-
-}
-void both(){
 
 }
 
+void generateNumbers(bool isSub, bool isAdd, int numbers[4]){
+    int signValue = getRandomSignValue();
+    int firstNum = getRandomNum();
+    int secondNum = getRandomNum();
+    int sum;
+
+
+    if((isSub && !isAdd) || isAdd && isSub){
+        if(isAdd){
+            signValue = getRandomSignValue();
+        }else{
+            signValue = 0;
+        }
+        if(secondNum > firstNum){
+            //swap numbers
+            if(secondNum > firstNum){
+                int temp = firstNum;
+                firstNum = secondNum;
+                secondNum = temp;
+            }
+        }
+    }else if(isAdd){
+        signValue = 1;
+    }
+
+    sum = (signValue == 1)? firstNum + secondNum : firstNum - secondNum;
+
+    numbers[0] = firstNum;
+    numbers[1] = secondNum;
+    numbers[2] = signValue; //if 1 then it's addition else if 0 it's subtraction
+    numbers[3] = sum;
+
+}
+
+long int getRandomNum(){
+    return rand() %100;
+}
+
+int getRandomSignValue(){
+    return rand() %2;
+}
