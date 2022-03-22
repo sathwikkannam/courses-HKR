792239e5eaeeb6ab56f668d60f182267559aa627
Sathwik <96546441+sathwikkannam@users.noreply.github.com>
2022-03-22 14:22:26 +0100

    Applied test mode

--- Programming for Embedded Systems/Labs/Lab 4/Lab4.h
+++ Programming for Embedded Systems/Labs/Lab 4/Lab4.h
@@ -5,14 +5,18 @@
 #ifndef LAB_4_LAB4_H
 #define LAB_4_LAB4_H
 
-#define numberOfQuestions 10
-#define numberofTestQuestions 15
+#define numberOfPracticeQuestions 10
+#define numberOfTestQuestions 15
 
-int getInput();
-void doTest();
-long int getRandomNum();
-void doPractice(bool isSub, bool isAdd);
+int getRandomNum();
+void doQuestions(bool isSub, bool isAdd, bool isTest);
 int getRandomSignValue();
-void generateNumbers(bool isSub, bool isBoth, int numbers[3]);
+void generateNumbers(bool isSub, bool isAdd, int numbers[4]);
+void printResponse(bool isGood, char goodResponses[6][24], char badResponses[4][24]);
+int promptForAnswer();
+void printTestResults(int first[], int second[], int correct[], int input[], char signs[], int percent);
+void storeValues(int firstNum, int secondNum, char sign, int sum, int input, int index);
+void printHeader(int percent);
+void loadResponses(char goodResponses[6][24], char badResponses[4][24]);
 
 #endif //LAB_4_LAB4_H
--- Programming for Embedded Systems/Labs/Lab 4/main.c
+++ Programming for Embedded Systems/Labs/Lab 4/main.c
@@ -1,86 +1,91 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 #include "Lab4.h"
 
 
 int main() {
     int practiceChoice;
-    int choice = getInput();
-
-    if(choice == 1){
-        printf("\n1. Additions\n2. Subtractions\n3. Additions and Subtraction\nEnter your choice:");
-        scanf("%d", &practiceChoice);
-
-        if(practiceChoice == 1){
-            doPractice(false, true);
-        }else if(practiceChoice == 2){
-            doPractice(true, false);
-        }else if(practiceChoice == 3){
-            doPractice(true, true);
-        }
-        main();
-    }else if(choice == 2){
-        doTest();
-        main();
-    }
-    return 0;
-}
-
-
-int getInput(){
     int choice;
 
     printf("1. Practices\n2. Test\n3. Exit\nEnter your choice:");
     scanf("%d", &choice);
 
 
-    return choice;
+    if(choice == 1){
+        printf("\n1. Additions\n2. Subtractions\n3. Additions and Subtraction\nEnter your choice:");
+        scanf("%d", &practiceChoice);
+
+        if(practiceChoice == 1){
+            doQuestions(false, true, false);
+        }else if(practiceChoice == 2){
+            doQuestions(true, false, false);
+        }else if(practiceChoice == 3){
+            doQuestions(true, true, false);
+        }
+        main();
+    }else if(choice == 2){
+        doQuestions(true, true, true);
+        main();
+    }
+    return 0;
 }
 
 
-void doPractice(bool isSub, bool isAdd){
-    int totalCorrectAnswers = 0;
+void doQuestions(bool isSub, bool isAdd, bool isTest){
+    int totalQuestions = (isTest)? numberOfTestQuestions : numberOfPracticeQuestions;
+    int i = 0; //represents how many questions have passed or how many correct answers;
     int answer;
-    int sum;
-    int firstNum;
-    int secondNum;
-    int signValue;
-    char sign;
     int numbers[4];
+    int firstNums[totalQuestions];
+    int secondNums[totalQuestions];
+    int sums[totalQuestions];
+    char signs[totalQuestions];
+    int inputs[totalQuestions];
+    char goodResponses[6][24];
+    char badResponses[4][24];
 
-    while(totalCorrectAnswers != numberOfQuestions){
+    loadResponses(goodResponses, badResponses);
+
+    while(i != totalQuestions){
         generateNumbers(isSub, isAdd, numbers);
-        firstNum = numbers[0];
-        secondNum = numbers[1];
-        signValue = numbers[2];
-        sum = numbers[3];
-        sign = (signValue == 1)? '+' : '-';
+        firstNums[i] = numbers[0];
+        secondNums[i] = numbers[1];
+        signs[i] = (char) numbers[2];
+        sums[i] = numbers[3];
 
-        printf("\n\t%d.\t%d %c %d = \n", totalCorrectAnswers+1, firstNum, sign, secondNum);
+        printf("\n\t%d.\t%d %c %d = \n", i+1, firstNums[i], signs[i], secondNums[i]);
 
-        while(answer != sum){
-            printf("Enter your answer: ");
-            scanf("%d", &answer);
-            printf("\n");
-
-            if(answer == sum){
-                totalCorrectAnswers++;
-            }else{
-                printf("No. Please try again.\n");
+        if(isTest){
+            answer = promptForAnswer();
+            inputs[i] = answer;
+            i++;
+        }else{
+            while(answer!= sums[i]){
+                answer = promptForAnswer();
+                if(answer == sums[i]){
+                    inputs[i] = answer;
+                    i++;
+                }else{
+                    printResponse(false, goodResponses, badResponses);
+                }
             }
+            printResponse(true, goodResponses, badResponses);
         }
 
     }
 
+    if(isTest){
+        printTestResults(firstNums, secondNums, sums, inputs, signs, i/totalQuestions);
+    }
 }
+
 
 void generateNumbers(bool isSub, bool isAdd, int numbers[4]){
     int signValue = getRandomSignValue();
     int firstNum = getRandomNum();
     int secondNum = getRandomNum();
-    int sum;
-
 
     if((isSub && !isAdd) || isAdd && isSub){
         if(isAdd){
@@ -100,19 +105,69 @@ void generateNumbers(bool isSub, bool isAdd, int numbers[4]){
         signValue = 1;
     }
 
-    sum = (signValue == 1)? firstNum + secondNum : firstNum - secondNum;
-
     numbers[0] = firstNum;
     numbers[1] = secondNum;
-    numbers[2] = signValue; //if 1 then it's addition else if 0 it's subtraction
-    numbers[3] = sum;
+    numbers[2] = (signValue == 1)? (int) '+' : (int) '-'; //if 1 then it's addition else if 0 it's subtraction
+    numbers[3] = (signValue == 1)? firstNum + secondNum : firstNum - secondNum;
 
 }
 
-long int getRandomNum(){
+
+void printResponse(bool isGood, char goodResponses[6][24], char badResponses[4][24]){
+    unsigned int randomIndex = (isGood)? rand() %6-1 : rand() %4-1;
+
+    if(isGood){
+        printf("%s\n", goodResponses[randomIndex]);
+    }else{
+        printf("%s\n", badResponses[randomIndex]);
+    }
+}
+
+void printTestResults(int first[], int second[], int correct[], int input[], char signs[], int percent){
+    printHeader(percent);
+    for (int i = 0; i < numberOfTestQuestions; i++) {
+        printf("%d\t%d %c %d%12d\t\t%d\n", i+1, first[i], signs[i], second[i], correct[i], input[i]);
+    }
+    printf("\n");
+}
+
+
+void printHeader(int percent){
+    printf("Your test result is %d (percentage)\nDetailed questions and answers:\n\n", percent);
+    printf("%s\t%s\t%s\t%s\n", "Nr", "Question", "Correct Answer", "Your answer");
+}
+
+
+
+int promptForAnswer(){
+    int answer;
+    printf("Enter your answer: ");
+    scanf("%d", &answer);
+    printf("\n");
+    return answer;
+}
+
+
+int getRandomNum(){
     return rand() %100;
 }
 
+
 int getRandomSignValue(){
     return rand() %2;
+}
+
+
+void loadResponses(char goodResponses[6][24], char badResponses[4][24]){
+    strcpy(goodResponses[0], "Very good!");
+    strcpy(goodResponses[1], "Excellent!");
+    strcpy(goodResponses[2], "Nice work!");
+    strcpy(goodResponses[3], "Well done!");
+    strcpy(goodResponses[4], "Great!");
+    strcpy(goodResponses[5], "Keep up the good work!");
+
+    strcpy(badResponses[0], "No. Please try again.");
+    strcpy(badResponses[1], "Wrong. Try once again.");
+    strcpy(badResponses[2], "Don’t give up!");
+    strcpy(badResponses[3], "No. Keep trying.");
 }
