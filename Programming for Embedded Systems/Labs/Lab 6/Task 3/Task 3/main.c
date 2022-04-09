d692e8059407f68773490a3e6afec95a1aeecd18
Sathwik <96546441+sathwikkannam@users.noreply.github.com>
2022-04-09 14:25:29 +0200

    Add comments

--- Programming for Embedded Systems/Labs/Lab 6/Task 3/Task 3/main.c
+++ Programming for Embedded Systems/Labs/Lab 6/Task 3/Task 3/main.c
@@ -9,7 +9,7 @@
 #include <avr/delay.h>
 #include <stdbool.h>
 
-#define F_CPU 16000000
+#define F_CPU 16000000UL
 
 bool isPressed(void);
 bool isLED = false;
@@ -18,19 +18,19 @@ int main(void)
 {
     /* Replace with your application code */
 	
-	DDRB = 0b00000010;
+	DDRB = 0b00000010; //set pin 1 as output and pin 0 as input
 	
     while (1) 
     {
 		
-		if(isPressed() && !isLED){
-			PORTB ^= (1<<1);
-			isLED = true;
-		}else if(isPressed() && isLED){
-			PORTB = 0x00;
-			isLED = false;
+		if(isPressed() && !isLED){ //if button pressed and led is not turned on.
+			PORTB ^= (1<<1); //5V to pin 1.
+			isLED = true; //set led is turned on.
+		}else if(isPressed() && isLED){ //if button pressed and led is turned on.
+			PORTB = 0x00; //0V to PORTB.
+			isLED = false; //set led is turned off.
 		}
-		_delay_ms(150);
+		_delay_ms(150); //delay 150ms.
 
     }
 	
@@ -40,11 +40,11 @@ int main(void)
 bool isPressed(void){
 	bool returnVal = false;
 	
-	if ((PINB & (1<<0))){
-		_delay_ms(25);
+	if ((PINB & (1<<0))){ //button pressed?
+		_delay_ms(25); //delay 25
 		
-		if ((PINB & (1<<0))){
-			returnVal = true;
+		if ((PINB & (1<<0))){ //check again.
+			returnVal = true; //its pressed.
 		}
 	}
 	
