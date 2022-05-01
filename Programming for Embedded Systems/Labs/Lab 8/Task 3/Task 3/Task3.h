ddf39b3d0a609e2fed46f807164aceeb6b67693d
Sathwik <96546441+sathwikkannam@users.noreply.github.com>
2022-05-01 18:25:31 +0200

    Add Task 3

---
+++ Programming for Embedded Systems/Labs/Lab 8/Task 3/Task 3/Task3.h
@@ -0,0 +1,16 @@
+/*
+ * Task3.h
+ *
+ * Created: 2022-05-01 18:15:49
+ *  Author: SAKA0191
+ */ 
+
+
+#ifndef TASK3_H_
+#define TASK3_H_
+
+
+
+
+
+#endif /* TASK3_H_ */
---
+++ Programming for Embedded Systems/Labs/Lab 8/Task 3/Task 3/main.c
@@ -0,0 +1,18 @@
+/*
+ * Task 3.c
+ *
+ * Created: 2022-05-01 18:05:15
+ * Author : SAKA0191
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
