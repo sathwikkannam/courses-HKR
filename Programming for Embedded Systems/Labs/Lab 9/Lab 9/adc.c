3eba535b3b7376967076f7df0efee28f301a313e
Sathwik <96546441+sathwikkannam@users.noreply.github.com>
2022-05-15 18:15:32 +0200

    Add rough structure of Lab 9

---
+++ Programming for Embedded Systems/Labs/Lab 9/Lab 9/adc.c
@@ -0,0 +1,53 @@
+/*
+ * adc.c
+ *
+ * Created: 2022-05-15 17:56:58
+ *  Author: SAKA0191
+ */ 
+
+
+#include "adc.h"
+
+volatile unsigned int adc_value;
+
+int main(){
+	
+	
+	while(1){
+		
+		
+	}
+	
+	return 0;
+}
+
+void adc_init(unsigned int mode){
+	ADMUX |= (1<<REFS0); //Set VREF to AVCC (+5V). 
+	ADCSRA = (1<<ADEN) | (1<<ADIE) | (1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0); //Enable ADC. Enable ADC conversion complete interrupt. 128 Prescaler.
+		
+	if(mode == 0){
+		ADMUX |= (1<<MUX1); //Enable MUX1 to listen to ADC2.
+		DIDR0 = (1<<ADC2D); //Disable digital input buffer for ADC2 pin.
+	}else if(mode == 1){
+		ADMUX |= (1<<MUX0); //Enable MUX1 to listen to ADC1.
+		DIDR0 = (1<<ADC1D); //Disable digital input buffer for ADC1 pin.
+	}else if(mode == 3){
+		DIDR0 = (1<<ADC0D); //Disable digital input buffer for ADC0 pin.
+	}
+		
+}
+
+void conversion_init(){
+	ADCSRA |= (1<<ADSC); // Enable Start conversion bit.
+}
+
+unsigned int get_ADC(){
+	return adc_value;
+}
+
+ISR(ADC_vect){
+	adc_value = ADC/4;
+}
+
+
+
---
+++ Programming for Embedded Systems/Labs/Lab 9/Lab 9/main.c
@@ -0,0 +1,89 @@
+/*
+ * Lab 9.c
+ *
+ * Created: 2022-05-15 16:40:31
+ * Author : SAKA0191
+ */ 
+
+ /* 
+		MUX REGISTER LAYOUT:
+		—————————————————————————————
+		| MUX3 | MUX2 | MUX1 | MUX0 |
+		—————————————————————————————
+	
+		MODE          |  PIN   | MUX 3 -> 0 
+		——————————————|————————|———————————
+		VOLT METER    |  ADC2  | 0010
+		SOIL HUMIDITY |  ADC1  | 0001
+		LIGHT METER   |  ADC0  | 0000		
+		
+		INSTRUMENT    |  PIN   | Direction register
+		——————————————|————————|———————————————————
+		BUTTON        |	 PIND2 | DDRD
+		LED           |  PINB2 | DDRB
+*/
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include "lcd.h"
+#include "lcd.c"
+#include "adc.h"
+#include "adc.c"
+
+volatile unsigned int mode = 0;
+
+int main(void)
+{
+	DDRB |= (1<<PINB2);
+	
+    while (1) 
+    {
+		button_init();
+		sei(); //Enable global interrupts (Set I bit in SREG).
+		adc_init(mode);
+		conversion_init();
+		
+		if(mode > 3){
+			mode = 0;
+		}
+    }
+}
+
+
+void button_init(){
+	DDRD ^= (1<<3); //set PIND 2 as output;
+	EICRA|= (1<<ISC01) | (1<<ISC00); // rising edge of INT0.
+	EIMSK|=(1<<INT0); //set external interrupt for PIND 2.
+	_delay_ms(25); //delay 25.
+}
+
+
+unsigned int toVoltage(unsigned int adc){
+	return (adc * 5)/1024; //5V is the reference Voltage.
+}
+
+
+unsigned char * toCharArray(unsigned int number){
+	char * numberArray = calloc(log10(number) + 1, sizeof(char));
+	
+	for (int i = log10(number); i >= 0; --i, number /= 10){
+		numberArray[i] = (number % 10) + '0';
+	}
+	
+	return numberArray;
+}
+
+
+
+
+ISR(ADC_vect){
+	//lcd puts.
+	conversion_init(); //Restart conversion.
+}
+
+
+ISR(INT0_vect){
+	 mode++;
+}
+
