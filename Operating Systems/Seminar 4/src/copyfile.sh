322f0505d7fd0ac0bc70e389bed141bd3d35b536
Sathwik <sathwik.kannam@gmail.com>
2022-10-16 16:35:12 +0200

    Remove spellcheck

--- Operating Systems/Seminar 4/src/copyfile.sh
+++ Operating Systems/Seminar 4/src/copyfile.sh
@@ -5,9 +5,7 @@ srcFile=$1
 desFile=$2
 
 
-
-# shellcheck disable=SC2086
-if [ -f "$srcFile" ] || [ -f $desFile ];
+if [ -f "$srcFile" ] || [ -f "$desFile" ];
   then
     while read -r line;
       do
