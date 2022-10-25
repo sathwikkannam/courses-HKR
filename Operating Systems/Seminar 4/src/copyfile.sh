191e0125827f41d2ac1d8447dc7763b7ea5eb268
Sathwik <sathwik.kannam@gmail.com>
2022-10-25 17:45:31 +0200

    Redundant file checking removed

--- Operating Systems/Seminar 4/src/copyfile.sh
+++ Operating Systems/Seminar 4/src/copyfile.sh
@@ -4,16 +4,7 @@
 srcFile=$1
 desFile=$2
 
-
-if [ -f "$srcFile" ] || [ -f "$desFile" ];
-  then
-    while read -r line;
-      do
-        echo "$line" >> "$desFile"
-      done <"$srcFile"
-  else
-    if ! [ -f "$desFile" ]
-      then
-        echo >> "$desFile"
-    fi
-fi
+while read -r line;
+  do
+    echo "$line" >> "$desFile"
+  done <"$srcFile"
