b9ef450dabd14744f66aaa2bcad2d0502b6d97a7
Sathwik <sathwik.kannam@gmail.com>
2022-11-25 17:37:08 +0100

    Minor changes

--- Computer Security/Labs/Lab 1 and 2/src/Lab_1.java
+++ Computer Security/Labs/Lab 1 and 2/src/Lab_1.java
@@ -9,13 +9,13 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.*;
-import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
-import java.security.cert.CertificateFactory;
 import java.util.Arrays;
 
 public class Lab_1 {
     private static Cipher decoder;
+    public static byte[] decryptedKey2;
+    public static byte[] decryptedCipherText;
 
     public static void main(String[] args){
         // Key1 - 128 bytes, IV - 128 bytes, Key2 - 128 bytes, ciphertext - remaining bytes.
@@ -24,29 +24,25 @@ public class Lab_1 {
         byte[] encryptedIV = Arrays.copyOfRange(originalData, 128, 256);
         byte[] encryptedKey2 = Arrays.copyOfRange(originalData, 256, 384);
         byte[] cipherText = Arrays.copyOfRange(originalData, 384, originalData.length);
-        PrivateKey privateKey = getPrivateKey("Ciphertext and Keys/lab1Store", "lab1EncKeys", "lab1StorePass".toCharArray(), "lab1KeyPass".toCharArray());
 
-        PublicKey publicKey = getPublicKey("Ciphertext and Keys/lab1Sign.cert");
+        PrivateKey privateKey = getPrivateKey("Ciphertext and Keys/lab1Store", "lab1EncKeys", "lab1StorePass".toCharArray(), "lab1KeyPass".toCharArray());
 
         byte[] decryptedKey1 = decryptRSA(privateKey, encryptedKey1);
         byte[] decryptedIV = decryptRSA(privateKey, encryptedIV);
-        byte[] decryptedKey2 = decryptRSA(privateKey, encryptedKey2);
-        byte[] decryptCipherText = decryptCipher(cipherText, "AES/CBC/PKCS5Padding", "AES", decryptedKey1, decryptedIV);
+        decryptedKey2 = decryptRSA(privateKey, encryptedKey2);
+        decryptedCipherText = decryptCipher(cipherText, "AES/CBC/PKCS5Padding", "AES", decryptedKey1, decryptedIV);
 
 
-        System.out.println("Cipher text: " + Arrays.toString(cipherText));
-        System.out.println("------------------------");
-        System.out.println("Plain text: " + Arrays.toString(decryptCipherText));
+        System.out.println("Plain text: \n------------------------\n" + new String(decryptedCipherText));
 
     }
 
     // Lab 1 - Task 1
-    public static byte[] decryptRSA(PrivateKey publicKey, byte[] encryptedData){
+    public static byte[] decryptRSA(PrivateKey key, byte[] encryptedData){
         try {
             decoder = Cipher.getInstance("RSA");
-            decoder.init(Cipher.DECRYPT_MODE, publicKey);
-            decoder.update(encryptedData);
-            return decoder.doFinal();
+            decoder.init(Cipher.DECRYPT_MODE, key);
+            return decoder.doFinal(encryptedData);
 
         } catch (NoSuchAlgorithmException | NoSuchPaddingException | IllegalBlockSizeException | BadPaddingException | InvalidKeyException e) {
             throw new RuntimeException(e);
@@ -80,23 +76,10 @@ public class Lab_1 {
     }
 
 
-    public static PublicKey getPublicKey(String file){
-        try{
-            FileInputStream publicKeyFile = new FileInputStream(file);
-            CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
-            Certificate certificate = certificateFactory.generateCertificate(publicKeyFile);
-            return certificate.getPublicKey();
-
-        }catch(FileNotFoundException | CertificateException e){
-            throw new RuntimeException(e);
-        }
-
-    }
-
     //https://docs.oracle.com/javase/7/docs/api/java/security/KeyStore.html
     public static PrivateKey getPrivateKey(String file, String alias, char[] filePassword, char[] keyPassword){
         try{
-            KeyStore keyStore = KeyStore.getInstance("JCEKS");
+            KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
             keyStore.load(new FileInputStream(file), filePassword);
             KeyStore.ProtectionParameter protParam = new KeyStore.PasswordProtection(keyPassword);
             KeyStore.PrivateKeyEntry pkEntry = (KeyStore.PrivateKeyEntry) keyStore.getEntry(alias,protParam);
