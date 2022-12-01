1312dfc3c2fe329aeba8a60ddd702fb661372e65
Sathwik <sathwik.kannam@gmail.com>
2022-12-01 20:55:11 +0100

    Finish Task 3

--- Algorithms and Data Structures/Seminars/Seminar 2/src/Task_3/Contact.java
+++ Algorithms and Data Structures/Seminars/Seminar 2/src/Task_3/Contact.java
@@ -1,3 +1,32 @@
 package Task_3;
 
-public record Contact (String name, String address) { }
+public class Contact{
+
+
+    private final String name;
+    private final String address;
+    private Contact next;
+
+    public Contact(String name, String address){
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public Contact getNext() {
+        return this.next;
+    }
+
+    public void setNext(Contact next) {
+        this.next = next;
+    }
+
+
+}
--- Algorithms and Data Structures/Seminars/Seminar 2/src/Task_3/MyLinkedList.java
+++ Algorithms and Data Structures/Seminars/Seminar 2/src/Task_3/MyLinkedList.java
@@ -1,6 +1,7 @@
 package Task_3;
 
-public class MyLinkedList<T> {
+public class MyLinkedList<T extends Contact> {
+    private T head;
 
     /*
         1. Add node
@@ -8,27 +9,72 @@ public class MyLinkedList<T> {
         3. Get node using index
      */
 
+    public void add(T item){
 
-    public MyLinkedList(){
+
+       if(head == null){
+           head = item;
+
+           // The new node's next pointer is always null.
+           item.setNext(null);
+
+       }else{
+
+           T node = head;
+
+           // We travel from head until a node is null
+           while(node.getNext() != null){
+               node = (T) node.getNext();
+           }
+
+           // When node is null -> the last node in LinkedList
+           node.setNext(item);
+       }
 
     }
 
 
+    public void remove(int index){
+        T node = head;
+        T nextNode; // Stores the node that "node" is pointing to.
 
-    public void add (T item){
+
+        for (int i = 0; i < index - 1; i++) {
+            node = (T) node.getNext();
+        }
+
+
+        nextNode = (T) node.getNext();
+        node.setNext(nextNode.getNext());
 
     }
 
 
-    public void remove (int index){
+    public Contact get(int index){
+        int i = 0;
+        T node;
 
+        for (node = head; node.getNext() != null && i != index; node = (T) node.getNext(), i++);
+
+        return node;
 
     }
 
 
-    public T getItem (int index){
+    @Override
+    public String toString(){
+        T node;
+        StringBuilder nodes = new StringBuilder();
 
-        return null;
+        for (node = head; node.getNext() != null ; node = (T) node.getNext()) {
+            nodes.append(String.format("[Name: %s, Address: %s]\n", node.getName(), node.getAddress()));
+        }
+
+
+        // Include the last node with null pointer.
+        nodes.append(String.format("[Name: %s, Address: %s]\n", node.getName(), node.getAddress()));
+
+        return nodes.toString();
     }
 
 }
