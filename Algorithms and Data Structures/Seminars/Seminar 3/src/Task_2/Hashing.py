31744e1f830ad08b49b2a2ab7eed8440b42db9ae
Sathwik <sathwik.kannam@gmail.com>
2022-12-23 23:05:39 +0100

    General Hashing class

--- Algorithms and Data Structures/Seminars/Seminar 3/src/Task_2/Hashing.py
+++ Algorithms and Data Structures/Seminars/Seminar 3/src/Task_2/Hashing.py
@@ -1,77 +1,64 @@
 from itertools import chain
 
 
-class Chaining:
-    def __init__(self, size):
-        self.hashTable = [[] for _ in range(size)]
+class Hashing:
+    def __init__(self, size, mode):
+        self.hash_table = [[] if mode == "CHAINING" else None for _ in range(size)]
         self.size = size
+        self.chaining = Chaining()
+        self.linear = LinearProbing()
+        self.quadratic = QuadraticProbing()
+        self.mode = mode
 
-    def insert(self, value):
-        self.hashTable[self.__hash__(value)].append(value)
+    def insert(self, key):
+        index = self.__hash__(key)
+        if self.mode.lower() == "CHAINING".lower():
+            self.chaining.insert(self.hash_table, key, index)
+        elif self.mode.lower() == "LINEAR".lower():
+            self.linear.insert(self.hash_table, key, index, self.size)
+        elif self.mode.lower() == "QUADRATIC".lower():
+            self.quadratic.insert(self.hash_table, key, index, self.size)
 
-    def __hash__(self, value):
-        return value % self.size
+    def __hash__(self, key=None):
+        return key % self.size
+
+    def get_mode(self):
+        return self.mode
 
     def __str__(self):
-        elements = ""
-        for i in self.hashTable:
-            elements.join(f"{i} ")
-            for j in i:
-                elements.join(f"--> {j} ")
+        return "".join(f"{x} --> {self.hash_table[x].__str__()}\n" for x in range(len(self.hash_table)))
+
+
+class Chaining:
+    def insert(self, hash_table, value, index):
+        hash_table[index].append(value)
 
 
 class LinearProbing:
-    def __init__(self, size):
-        self.hashTable = [None] * size
-        self.size = size
-
-    def insert(self, value):
-        index = self.__hash__(value)
-
-        if self.hashTable[index] is None:
-            self.__insert(index, value)
+    def insert(self, hash_table, value, index, size):
+        if hash_table[index] is None:
+            hash_table[index] = value
             return
 
         # Iterates from index -> size then 0 -> index
-        for i in chain(range(index, self.size), range(0, index + 1)):
-            if self.hashTable[i] is None:
-                self.__insert(i, value)
+        for i in chain(range(index, size), range(0, index + 1)):
+            if hash_table[i] is None:
+                hash_table[i] = value
                 return
-
-    def __insert(self, index, value):
-        self.hashTable[index] = value
-
-    def __hash__(self, value):
-        return value % self.size
-
-    def __str__(self):
-        return "".join(f"{i}, " for i in self.hashTable)
 
 
 class QuadraticProbing:
-    def __init__(self, size):
-        self.hashTable = [None] * size
-        self.size = size
-
-    def insert(self, value):
-        index = self.__hash__(value)
-
-        if self.hashTable[index] is None:
-            self.hashTable[index].append(value)
+    def insert(self, hash_table, value, index, size):
+        if hash_table[index] is None:
+            hash_table[index] = value
             return
 
-        for i in range(self.size):
-            index = self.__quad_hash(i, value)
+        for i in range(size):
+            index = self.__quad_hash(i, value, size)
 
-            if self.hashTable[index] is None:
-                self.hashTable[index].append(value)
+            if hash_table[index] is None:
+                hash_table[index] = value
                 return
 
-    def __hash__(self, value):
-        return value % self.size
-
-    def __quad_hash(self, i, value):
-        return (value + pow(i, 2)) % self.size
-
-    def __str__(self):
-        return "".join(f"{i}, " for i in self.hashTable)
+    def __quad_hash(self, i, value, size):
+        return (value + pow(i, 2)) % size
---
+++ Algorithms and Data Structures/Seminars/Seminar 3/src/Task_2/Main.py
@@ -0,0 +1,19 @@
+from Hashing import *
+
+
+def main():
+    test_case = [int(x) for x in input("Enter elements separated by ',':").split(",")]
+    size = int(input("Size: "))
+
+    modes = ["CHAINING", "LINEAR", "QUADRATIC"]
+
+    for mode in modes:
+        hashing = Hashing(size, mode)
+        for item in test_case:
+            hashing.insert(item)
+
+        print(f"{hashing.get_mode()}: \n{hashing.__str__()}")
+
+
+if __name__ == '__main__':
+    main()
