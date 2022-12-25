67752a5c5695e2fadc01aa48d0e689c04753f5e7
Sathwik <sathwik.kannam@gmail.com>
2022-12-25 18:45:15 +0100

    Add comments

--- Algorithms and Data Structures/Seminars/Seminar 3/src/Task_1/BinaryHeap.py
+++ Algorithms and Data Structures/Seminars/Seminar 3/src/Task_1/BinaryHeap.py
@@ -14,19 +14,26 @@ class BinaryHeap:
 
     def __init__(self, size):
         self.__heap = [0] * (size + 1)
-        self.index = 0
+        self.__index = 0
 
-    # Add to the end of the heap.
-    # Then check from key -> root to heapify up.
     def insert(self, key):
-        self.__heap[self.index] = key
-        self.index += 1
-        self.__min_heapify(self.index - 1)
+        """
+        Add to the end of the heap.
+        Then check from key -> root to heapify up.
+        :param key: The value of a node
+        """
+        self.__heap[self.__index] = key
+        self.__index += 1
+        self.__min_heapify(self.__index - 1)
 
-    # Check from bottom node to the root.
-    # If the child has a parent and the parent's key is > last node: swap them,
-    # Then jump to its parent node and repeat until a node has no parent.
     def __min_heapify(self, last_child):
+        """
+        Check from bottom node to the root.
+        If the child has a parent and the parent's key is > last node: swap them,
+        Then jump to its parent node and repeat until a node has no parent.
+        :param last_child: The last inserted node
+        :return: None
+        """
         if not self.__has_parent(last_child):
             return
 
@@ -35,16 +42,27 @@ class BinaryHeap:
             self.__min_heapify(get_parent_index_of(last_child))
 
     def __get_parent_of(self, child_index):
+        """
+        :param child_index: An index of a child node.
+        :return: Parent node
+        """
         return self.__heap[get_parent_index_of(child_index)]
 
     def __has_parent(self, child_index):
+        """
+        :param child_index: An index of a child node.
+        :return: A boolean if a child node has parent
+        """
         return get_parent_index_of(child_index) >= 0
 
     def __swap(self, a, b):
         self.__heap[a], self.__heap[b] = self.__heap[b], self.__heap[a]
 
-    # Change the order to fit a traversal order
     def __str__(self):
+        """
+        Change the order to fit a traversal order
+        :return: None
+        """
         return "".join(
             f"Parent: {self.__heap[i]} \t "
             f"Left Child: {self.__heap[get_left_child_index(i)]} \t "
