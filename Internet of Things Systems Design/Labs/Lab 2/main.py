9685890f1c2490ef83458cafc3aca74fa4aa3fb3
Sathwik <96546441+sathwikkannam@users.noreply.github.com>
2023-12-06 19:35:52 +0100

    Lab 3

--- Internet of Things Systems Design/Labs/Lab 2/main.py
+++ Internet of Things Systems Design/Labs/Lab 2/main.py
@@ -6,46 +6,39 @@ from azure.iot.device import IoTHubDeviceClient, Message
 CONNECTION_STRING = os.getenv("CONNECTION_STRING")
 
 TEMPERATURE = 20.0
-MSG_TXT = '{{"temperature": {temperature}}}'
+JSON_MESSAGE = '{{"temperature": {temperature}}}'
 
 
-def run_telemetry_sample(client):
-    # This sample will send temperature telemetry every second
-    print("IoT Hub device sending periodic messages")
+def main():
+    client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
 
-    # Connect to Azure client
+    try:
+        print("Starting to send telemetry")
+        send_temperature_telemetry(client)
+    except KeyboardInterrupt:
+        print("Stopped sending telemetry")
+    finally:
+        client.shutdown()
+
+
+def send_temperature_telemetry(client):
+    """
+    This function sends a temperature (simulated) to the Azure IoT Hub repeatedly.
+    Sending the message in JSON enables the routing pipelines to auto-parse the body of the message.
+    :param client: Azure IoT hub client initialized with a primary connecting string of this device.
+    """
+
     client.connect()
 
     while True:
-        # Build the message with simulated telemetry values.
         temperature = TEMPERATURE + (random.random() * 15)
-        msg_txt_formatted = MSG_TXT.format(temperature=temperature)
+        msg_txt_formatted = JSON_MESSAGE.format(temperature=temperature)
         message = Message(msg_txt_formatted)
 
-        # Send the message.
         print("Sending message: {}".format(message))
         client.send_message(message)
         print("Message successfully sent")
         time.sleep(10)
-
-
-def main():
-    print("IoT Hub Quickstart #1 - Simulated device")
-    print("Press Ctrl-C to exit")
-
-    # Instantiate the client. Use the same instance of the client for the duration of
-    # your application
-    client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
-
-    # Run Sample
-    try:
-        run_telemetry_sample(client)
-    except KeyboardInterrupt:
-        print("IoTHubClient sample stopped by user")
-    finally:
-        # Upon application exit, shut down the client
-        print("Shutting down IoTHubClient")
-        client.shutdown()
 
 
 if __name__ == '__main__':
